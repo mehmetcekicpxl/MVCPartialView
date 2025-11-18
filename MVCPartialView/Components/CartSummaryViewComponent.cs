abbb87cfc7a036f6a43233b350f2026f98d6e459
MehmetCekicPXL <mehmet.cekic@student.pxl.be>
2025-11-18 11:14:19 +0100

    Add project files.

---
+++ MVCPartialView/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCPartialView.Components
+{
+    public class CartSummaryViewComponent :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            // fake data
+            int productCount = 3;
+            decimal totalPrice = 2970;
+
+            return View((productCount, totalPrice));
+        }
+    }
+}
---
+++ MVCPartialView/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCPartialView.Models;
+
+namespace MVCPartialView.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+            {
+                new Product { Id=1, Name="Laptop", Price=2500 },
+                new Product { Id=2, Name="Mouse", Price=120 },
+                new Product { Id=3, Name="Toetsenbord", Price=350 },
+            };
+
+            return View(products);
+        }
+    }
+}
